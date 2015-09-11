68ebc5a769583a823ddbd0deca1d1a636bae1d02
Perry <chenzhipeng901105@gmail.com>
2015-09-11 18:42:53 +0800

    add codes

---
+++ UICollectionViewFlickrSearch/FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,16 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by PerryChen on 9/11/15.
+//  Copyright (c) 2015 PerryChen. All rights reserved.
+//
+
+import UIKit
+
+class FlickrPhotosViewController: UICollectionViewController {
+
+    private let reuseIdentifier = "FlickrCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0 )
+    
+}
